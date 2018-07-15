bdc79f9fd075334332d8a0a73436ed02d190a4c1
LeonLinuxNerd <1206225794@qq.com>
2018-07-15 15:54:07 +0800

    leetcode88

---
+++ leetcode88.c
@@ -0,0 +1,42 @@
+给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+
+说明:
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+示例:
+
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
+
+void merge(int* nums1, int m,int* nums2,int n){
+	int p1 = m-1;
+	int p2 = n-1;
+	int i;
+	if(n = 0){
+		return;
+	}
+	if(m==0){
+		for(i=0;i<n;i++){
+			num1[i]=nums2[i];
+		}
+	}
+	for(i=n+m-1;i>=0&&p1>=0&&p2>=0;i--){
+		if(nums1[p1]>nums2[p2]){
+			nums1[i]=nums1[p1];
+			p1--;
+		}else{
+			nums1[p1]=nums[p2];
+			p2--;
+		}
+	}
+	if(p1<0){
+		while(p2>=0){
+			nums2[i--]=nums[p2--];
+		}
+		return;
+	}
+}
