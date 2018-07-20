0e7b39de0a5b3a23f6b02e6199857bb11049b112
LeonLinuxNerd <1206225794@qq.com>
2018-07-20 17:16:25 +0800

    leetcode219

---
+++ leetcode219.c
@@ -0,0 +1,25 @@
+给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+示例 1:
+
+输入: nums = [1,2,3,1], k = 3
+输出: true
+示例 2:
+
+输入: nums = [1,0,1,1], k = 1
+输出: true
+示例 3:
+
+输入: nums = [1,2,3,1,2,3], k = 2
+输出: false
+
+bool containsNearbyDuplicate(int* nums, int numsSize, int k){
+	int i,j;
+	for(i=0; i<numsSize; i++){
+		for(j=i+1; j<numsSize; j++){
+			if((nums[i]==nums[j]&&((j-i)<=k))
+				return true;
+		}
+	}
+	return false;
+}
