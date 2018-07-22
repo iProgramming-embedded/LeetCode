a5b52b11fed5a313289e8138bf4ceb8d39954580
LeonLinuxNerd <1206225794@qq.com>
2018-07-22 16:01:21 +0800

    leetcode28

---
+++ leetcode28.c
@@ -0,0 +1,48 @@
+实现 strStr() 函数。
+
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1:
+
+输入: haystack = "hello", needle = "ll"
+输出: 2
+示例 2:
+
+输入: haystack = "aaaaa", needle = "bba"
+输出: -1
+说明:
+
+当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+
+对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+
+int strStr(char* haystack, char* needle) {
+   // if(needle = '\0')
+   //     return 0;
+    int hIndex=0,nIndex=0;  
+    int hLength = strlen(haystack);
+    int nLength = strlen(needle);
+    if(nLength==0)
+        return 0;
+
+    int res = -1;
+    while(hIndex<hLength&&nIndex<nLength){
+        if(haystack[hIndex]==needle[nIndex]){
+            if(nIndex==0)
+                res = hIndex;
+            hIndex++;
+            nIndex++;
+        }
+        else{
+            hIndex++;
+            nIndex = 0;
+            if(res!=-1){
+                hIndex = res+1;
+                res = -1;
+            }
+        }
+    }
+    if(nIndex==nLength)
+        return res;
+    return -1;
+}
